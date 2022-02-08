250172cfa9752b4dc9f32754da98d65724581d2c
A2ZTestAutomation <corpdevops@gmail.com>
2022-02-08 10:42:58 +0530

    Updated Dashboard details

--- cypress/integration/examples/sample.spec.js
+++ cypress/integration/examples/sample.spec.js
@@ -4,6 +4,7 @@ describe('First Visual testing sample', () => {
     it('Sample snapshot test', () => {
         cy.visit('http://example.com')
         cy.viewport('ipad-2')
+        expect(true).to.equal(false)
         //To compare the screen
         //   cy.viewport('iphone-6')
             //  cy.viewport('ipad-2')
